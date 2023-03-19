ce43ede4363e62737a2586cd047bfb2a5a2170e2
jjosei <oseisarfohjesse@gmail.com>
2023-03-19 22:57:13 +0000

    A application that is a store front for any type of item imaginable

---
+++ src/Books.java
@@ -0,0 +1,11 @@
+public class Books extends ProductForSale{
+
+    public Books(String type, double price, String description) {
+        super(type, price, description);
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("This book is of " + type +"type.");
+    }
+}
--- src/ProductForSale.java
+++ src/ProductForSale.java
@@ -11,4 +11,6 @@ public abstract class ProductForSale {
     public double getPrice(int qty){
         return (price * qty);
     }
+
+    public abstract void showDetails();
 }
